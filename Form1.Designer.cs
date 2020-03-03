ede9a2860d7b03b47080b0d6daf276b383af3f90
MRX7999 <rubanoff.and@yandex.by>
2020-03-03 20:48:55 +0300

    Power by AnRub

---
+++ Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Может_быть_нормальный_файловый_менеджер
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(23, 115);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(299, 310);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(253, 67);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(69, 23);
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "Перейти";
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 67);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(23, 96);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(44, 13);
+            this.metroButton2.TabIndex = 4;
+            this.metroButton2.Text = "<";
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 435);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Style = MetroFramework.MetroColorStyle.Green;
+            this.Text = "Power by Andrey Rubanov";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.TextBox textBox1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using MetroFramework.Components; //Делаем всеми любимый графений
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics; // подключаем библиотеку для открытия файлов
+using System.IO; //подключаем библиотеку для юзания в дисковом пространстве
+
+namespace Может_быть_нормальный_файловый_менеджер
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MetroStyleManager metroStyleManager1 = null; //Оживляем графений, если вылезает ошибка удалить и вставить за ного эти две строки
+            this.StyleManager = metroStyleManager1;
+        }
+
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear(); // фикс бага с наслойкой директорий друг на друга
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text); // передаем путь в конструктор
+            DirectoryInfo[] dirs = dir.GetDirectories(); // Масив для хранения катологов
+            foreach (DirectoryInfo ssDir in dirs) // перебор элементов масива
+            {
+                listBox1.Items.Add(ssDir); // добовляем в список элемент
+            }
+
+            FileInfo[] files = dir.GetFiles(); // отображаем сами файлы
+            foreach (FileInfo ssFile in files) // выводим все содержащиеся файлы на экран
+            {
+                listBox1.Items.Add(ssFile); // вывод
+            }
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e) // пользовательский клик
+        {
+            if (Path.GetExtension(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString())) == "") //открытие файла
+            {
+                textBox1.Text = Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString()); //выебнулся
+                listBox1.Items.Clear(); // фикс бага с наслойкой директорий друг на друга
+                DirectoryInfo dir = new DirectoryInfo(textBox1.Text); // передаем путь в конструктор
+                DirectoryInfo[] dirs = dir.GetDirectories(); // Масив для хранения катологов
+                foreach (DirectoryInfo ssDir in dirs) // перебор элементов масива
+                {
+                    listBox1.Items.Add(ssDir); // добовляем в список элемент
+                }
+
+                FileInfo[] files = dir.GetFiles(); // отображаем сами файлы
+                foreach (FileInfo ssFile in files) // выводим все содержащиеся файлы на экран
+                {
+                    listBox1.Items.Add(ssFile); // вывод
+                }
+            }
+    else
+            {
+                Process.Start(Path.Combine(textBox1.Text, listBox1.SelectedItem.ToString())); // Дефолтный вывод
+            }
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e) // описываем кнопку назад
+        {
+            if (textBox1.Text[textBox1.Text.Length - 1] == '\\') // стриаем сеши
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1); // удаляем один символ
+                while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+
+                }
+            }
+            else if (textBox1.Text[textBox1.Text.Length - 1 ] != '\\' )
+            {
+                while (textBox1.Text[textBox1.Text.Length - 1] != '\\')
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+
+                }
+            }
+            listBox1.Items.Clear(); // фикс бага с наслойкой директорий друг на друга
+            DirectoryInfo dir = new DirectoryInfo(textBox1.Text); // передаем путь в конструктор
+            DirectoryInfo[] dirs = dir.GetDirectories(); // Масив для хранения катологов
+            foreach (DirectoryInfo ssDir in dirs) // перебор элементов масива
+            {
+                listBox1.Items.Add(ssDir); // добовляем в список элемент
+            }
+
+            FileInfo[] files = dir.GetFiles(); // отображаем сами файлы
+            foreach (FileInfo ssFile in files) // выводим все содержащиеся файлы на экран
+            {
+                listBox1.Items.Add(ssFile); // вывод
+            }
+
+        }
+    }
+}
